747d2356cafe2a69c953c01fbc45cdfbfbe0b69a
satheesheppalapelli <eppalapellisatheesh1@gmail.com>
2021-10-27 11:39:14 -0500

    spring initializr

---
+++ src/main/java/edu/nwmissouri/zoo05group/ZooAppApplication.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo05group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nwmissouri/zoo05group/ZooAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo05group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
