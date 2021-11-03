cf639ca1b7b0bec18b13a2f2b98a6fc9d8301798
Saikrishna Vuppala <S545040@nwmissouri.edu>
2021-11-03 11:47:41 -0500

    Update StringRay.java

--- src/main/java/edu/nwmissouri/zoo05group/StringRay.java
+++ src/main/java/edu/nwmissouri/zoo05group/StringRay.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.nwmissouri.saikrishna04lab;
+package edu.nwmissouri.zoo04lab;
 
 /**
- *
+ * Provides methods to create and run a group of StringRay.
  * @author Saikrishna Vuppala
  */
 public class StringRay extends Animal {
@@ -18,7 +18,10 @@ public class StringRay extends Animal {
     public StringRay(String name) {
         super(name);
     }
-
+    
+   /**
+   * Swim method
+   */
    // @Override
     public void swim() {
         System.out.println("Generic animal swim");
@@ -28,8 +31,45 @@ public class StringRay extends Animal {
     public void move() {
         System.out.println("Generic animal movement");
     }
- @Override
+    @Override
     public void speak() {
         System.out.println("Generic animal voice");
     }
+    
+    
+    public void confess() {
+        double a = 3.5;
+        int b = 3;
+        double c = getStringRayAddition(a, b);
+        System.out.printf("I know FloppyAddition! %4.2f plus %d is %4.2f \n", a, b, c);
+    }
+
+    public double getStringRayAddition(double valueOne, int valueTwo) {
+    return valueOne + valueTwo;
+    }
+
+    /**
+    * Enum function with plant values
+    *
+    */
+    public enum Times {
+        MORNING,
+        EVENEING,
+        NIGHT;
+    }
+
+    /**
+    * Main method
+    * @param args
+    */
+    public static void main(String[] args) {
+        var sr = new StringRay("Sai");
+        sr.speak();
+        sr.move();
+        sr.swim();
+        for (Times times : Times.values()) {
+            System.out.println(times);
+        }
+    }
+    
 }
