a5c598c4cbaf0e53e906e603526efac2a9ec3080
satheesheppalapelli <eppalapellisatheesh1@gmail.com>
2021-11-05 12:44:39 -0500

    updated the files

--- src/main/java/edu/nwmissouri/zoo05group/AbstractGame.java
+++ src/main/java/edu/nwmissouri/zoo05group/AbstractGame.java
@@ -2,12 +2,30 @@ package edu.nwmissouri.zoo05group;
 
 /**
  *
- * @author Satheesh Eppalapelli
+ * @author S545243 Satheesh Eppalapelli
  */
 public abstract class AbstractGame implements Runnable {
 
-    private String game_name;
-    private String number_of_players;
+    /**
+     * Every game has a name. Class variable name is not private, but protected
+     * - only subclasses can use it.
+     */
+    protected String game_name = "no name";
+     /**
+     * Every game has a players. Class variable number_ofPlayers is not private, but protected
+     * - only subclasses can use it.
+     */
+    protected int number_of_players = 0;
+
+    /**
+     *
+     * @param game_name
+     * @param number_of_players
+     */
+    AbstractGame(String game_name, int number_of_players) {
+        this.game_name = game_name;
+        this.number_of_players = number_of_players;
+    }
 
     public String getGame_name() {
         return game_name;
@@ -17,11 +35,11 @@ public abstract class AbstractGame implements Runnable {
         this.game_name = game_name;
     }
 
-    public String getNumber_of_players() {
+    public int getNumber_of_players() {
         return number_of_players;
     }
 
-    public void setNumber_of_players(String number_of_players) {
+    public void setNumber_of_players(int number_of_players) {
         this.number_of_players = number_of_players;
     }
 
@@ -30,13 +48,39 @@ public abstract class AbstractGame implements Runnable {
      * @param name
      * @return
      */
-    public abstract String game(String name);
+    public String game(String name) {
+        return name;
+    }
 
-    public abstract void gamePlay();
+    /**
+     *
+     * call this play() method to see which game you are playing.
+     */
+    public void play() {
+        System.out.println("Which game do you want to play!");
+    }
 
-    public abstract void skill();
+    /**
+     *
+     * call this skill() method to see all the skill for the game
+     */
+    public void skill() {
+        System.out.println("What are the skill do you have for the gameplay!");
+    }
 
-    public abstract void rules();
+    /**
+     *
+     * call this rules() method to see all the rules for the game
+     */
+    public void rules() {
+        System.out.println("What are the rules for the gameplay!");
+    }
 
-    public abstract void scoring();
+    /**
+     *
+     * call this scoring() method to see all the scoring for the game
+     */
+    public void scoring() {
+        System.out.println("Scoring for the gameplay!");
+    }
 }
---
+++ src/main/java/edu/nwmissouri/zoo05group/Badminton.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo05group;
+
+/**
+ *
+ * @author S545243 Satheesh Eppalapelli
+ */
+public abstract class Badminton extends AbstractGame {
+
+    /**
+     *
+     * @param game_name
+     * @param number_of_players
+     */
+    Badminton(String game_name, int number_of_players) {
+        super(game_name, number_of_players);
+    }
+
+    /**
+     *
+     * @param name
+     * @return name
+     *
+     * call this play() method to see which game you are playing.
+     */
+    public String game(String name) {
+        return name;
+    }
+
+    /**
+     *
+     * call this play() method to see which game you are playing.
+     */
+    @Override
+    public void play() {
+
+    }
+
+    /**
+     *
+     * call this skill() method to see which game you are playing.
+     */
+    @Override
+    public void skill() {
+
+    }
+
+    /**
+     *
+     * call this rules() method to see which game you are playing.
+     */
+    @Override
+    public void rules() {
+
+    }
+
+    /**
+     *
+     * call this scoring() method to see which game you are playing.
+     */
+    @Override
+    public void scoring() {
+
+    }
+}
