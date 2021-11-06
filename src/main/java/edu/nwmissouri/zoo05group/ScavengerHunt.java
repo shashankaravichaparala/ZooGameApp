d8921c72961571c3e9debd597a3b211382eb17e9
Saikrishna Vuppala <S545040@nwmissouri.edu>
2021-11-05 23:16:37 -0500

    Update ScavengerHunt.java

--- src/main/java/edu/nwmissouri/zoo05group/ScavengerHunt.java
+++ src/main/java/edu/nwmissouri/zoo05group/ScavengerHunt.java
@@ -6,35 +6,36 @@ package edu.nwmissouri.zoo05group;
 
 /**
  *
- * @author S545040 Sai Krishna Vuppala
+ * @author S545040 
  */
 public class ScavengerHunt extends AbstractGame {
-
 
     /**
      *
      * @param game_name
      * @param number_of_players
      */
-    public Bingo(String game_name, int number_of_players) {
+    public ScavengerHunt(String game_name, int number_of_players) {
         super(game_name, number_of_players);
     }
 
     /**
      *
      * @param game_name
+     *
      * call this play() method to see which game you are playing.
      */
+    
     @Override
     public void game(String game_name) {
-        System.out.printf("Playing %s\n", this.game_name);
+          System.out.printf("I Like to play %s. \n", this.game_name);
     }
 
     /**
      * call this player() method to see how many number of players are playing.
      */
     public void players() {
-        System.out.printf("Playing ScavengerHunt with %s\n", this.number_of_players);
+        System.out.printf("Playing scavengerHunt with %s players\n", this.number_of_players);
     }
 
     /**
@@ -44,7 +45,6 @@ public class ScavengerHunt extends AbstractGame {
     @Override
     public void play() {
         System.out.println("ScavengerHunt is an engaging game played by children and adults alike. To play the game, divide the players into 2 teams, and give them the first clue. Once the players find the first item, they can locate the second with help from the next clue. When the time limit runs out, count the items collected from both teams");
-
     }
 
     /**
@@ -53,7 +53,8 @@ public class ScavengerHunt extends AbstractGame {
      */
     @Override
     public void skill() {
-        System.out.println(" ScavengerHunts allow us to practice problem-solving in a tangible way. It helps to reinforce and act-out methods they have been taught by parents or teachers in a physical way, leading to increased retention of the lessons ");
+        System.out.println("ScavengerHunts allow us to practice problem-solving in a tangible way. It helps to reinforce and act-out methods they have been taught by parents or teachers in a physical way, leading to increased retention of the lessons ");
+
     }
 
     /**
@@ -62,7 +63,8 @@ public class ScavengerHunt extends AbstractGame {
      */
     @Override
     public void rules() {
-        System.out.println(" You must find all items and snap a photo. Once the timer ends, whoever has the most pictures wins. If participating as a team, you must always stay together");
+        System.out.println("You must find all items and snap a photo. Once the timer ends, whoever has the most pictures wins. If participating as a team, you must always stay together");
+
     }
 
     /**
@@ -74,29 +76,30 @@ public class ScavengerHunt extends AbstractGame {
         System.out.println("Players earn “points” for each task completed within a certain time frame. Scavenger hunt prizes are awarded to the team with the highest score ");
     }
 
-     void game() {
+    void game() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-     
+
+   
+
     /**
      * Main method
      *
      * @param args
      */
     public static void main(String[] args) {
-        ScavengerHunts sh = new ScavengerHunt("ScavengerHunt", 2);
-        sh.game("ScavengerHunt");
-        sh.players();
-        sh.rules();
-        sh.play();
-        sh.skill();
-        sh.scoring();
-
+        ScavengerHunt scavengerHunt = new ScavengerHunt("scavengerHunt", 2);
+        scavengerHunt.game("scavengerHunt");
+        scavengerHunt.players();
+        scavengerHunt.rules();
+        scavengerHunt.play();
+        scavengerHunt.skill();
+        scavengerHunt.scoring();
     }
 
     /**
-     * An instance of Runnable - call this run() method to
-     * see all the ScavengerHunt tricks.
+     * An instance of an scavengerHunt is now Runnable - call this run() method to
+     * see all the scavengerHunt tricks.
      */
     @Override
     public void run() {
